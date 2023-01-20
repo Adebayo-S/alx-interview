46ac696748f105161b93633b0c56c08461c42bc2
Adebayo Samuel <eladebayoor@gmail.com>
2023-01-19 23:46:19 -0500

    [fix]: main: script embedded in main

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -17,18 +17,19 @@ def printlog(status, file_size) -> None:
         print(f"{k}: {v}")
 
 
-try:
-    for line in sys.stdin:
-        match = re.match(
-            r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*)\] "(.*)" (\d+) (\d+)', line)
-        if match:
-            count += 1
-            file_size += int(match.group(5))
-            status_code = match.group(4)
-            if status_code and type(eval(status_code)) == int:
-                status[status_code] = status[status_code] + \
-                    1 if status_code in status else 1
-            if count % 10 == 0:
-                printlog(status, file_size)
-except KeyboardInterrupt:
-    printlog(status, file_size)
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            match = re.match(
+                r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*)\] "(.*)" (\d+) (\d+)', line)
+            if match:
+                count += 1
+                file_size += int(match.group(5))
+                status_code = match.group(4)
+                if status_code and type(eval(status_code)) == int:
+                    status[status_code] = status[status_code] + \
+                        1 if status_code in status else 1
+                if count % 10 == 0:
+                    printlog(status, file_size)
+    except KeyboardInterrupt:
+        printlog(status, file_size)
