3cb7126d1d48494a233ecd96c7c817b3b9c34809
Adebayo Samuel <eladebayoor@gmail.com>
2023-01-26 11:45:59 -0500

    [fix]: added a check for invalid byte sequences

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,11 +4,18 @@ determines if a given data
 set represents a valid UTF-8 encoding
 """
 
+import re
+
 
 def validUTF8(data):
     """validate data for UTF-8"""
+    rgx = b'^(?:[\x00-\x7F]|[\xC2-\xDF][\x80-\xBF]|\xE0[\xA0-\xBF][\x80-\xBF]|[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}|\xED[\x80-\x9F][\x80-\xBF]|\xF0[\x90-\xBF][\x80-\xBF]{2}|[\xF1-\xF3][\x80-\xBF]{3}|\xF4[\x80-\x8F][\x80-\xBF]{2})*$'
     try:
-        bytes(data).decode('utf-8')
+        byte_data = bytes(data)
+        # 1. Add a check for invalid byte sequences
+        if not re.match(rgx, byte_data):
+            return False
+        byte_data.decode('utf-8')
     except BaseException:
         return False
     return True
